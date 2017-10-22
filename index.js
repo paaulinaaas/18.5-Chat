46c714026162b66ee02115465a90131cc5b8ef4b
Paulina Sikora <paaulinaaas@gmail.com>
2017-10-22 21:01:16 +0200

    fixed

--- index.js
+++ index.js
@@ -12,6 +12,34 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function(socket) {				 
+	socket.on("join", function(name){
+	  userService.addUser({
+	    id: socket.id,
+	    name
+	  });
+	  
+	  io.emit('update', {
+	    users: userService.getAllUsers()
+	  });
+	});
+
+	socket.on('disconnect', () => {
+	    userService.removeUser(socket.id);
+	    socket.broadcast.emit('update', {
+	      users: userService.getAllUsers()
+	    });
+  	});
+
+  	socket.on('message', function(message){
+	    const {name} = userService.getUserById(socket.id);
+	    socket.broadcast.emit('message', {
+	      text: message.text,
+	      from: name
+	    });
+	 });
+});
+
 server.listen(3000, function(){
   console.log('listening on *:3000');
 });
